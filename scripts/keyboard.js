3b063a3beabc0416f666911cd8ac4e7f5c771800
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-11-24 23:53:13 +1100

    Implemented basic key handler
    - Handler simply sets a global flag for the current state of each button
    
    - Arrows
    	- Up
    	- Down
    	- Left
    	- Right

---
+++ scripts/keyboard.js
@@ -0,0 +1,26 @@
+var leftPressed = false;
+var rightPressed = false;
+var downPressed = false;
+var upPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+        case 37: leftPressed = true; break;
+        case 38: upPressed = true; break;
+        case 39: rightPressed = true; break;
+        case 40: downPressed = true; break;
+    }
+}
+
+function keyUpHandler(e) {
+    switch(e.keyCode) {
+        case 37: leftPressed = false; break;
+        case 38: upPressed = false; break;
+        case 39: rightPressed = false; break;
+        case 40: downPressed = false; break;
+    }
+}

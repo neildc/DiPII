6ed92ca5f8fb987cf7b296b3351d219e9c085c8c
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-12-02 00:41:43 +1100

    wait for textures to load

--- scripts/textures.js
+++ scripts/textures.js
@@ -1,18 +1,63 @@
+var stanLogo;
+var amazonLogo;
+var netflixLogo;
+var dollarBill;
+var dude;
+var fire;
 
-var stanLogo = new Image();
-stanLogo.src = 'res/stansmall.png';
+function loadImages(sources, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+    // get num of sources
+    for(var src in sources) {
+        numImages++;
+    }
+    for(var src in sources) {
+        images[src] = new Image();
+        images[src].onload = function() {
+            if(++loadedImages >= numImages) {
+                callback(images);
+            }
+        };
+        images[src].src = sources[src];
+    }
+}
 
-var amazonLogo = new Image();
-amazonLogo.src = 'res/amazon_logo.png';
+var sources = {
+    stanLogo: 'res/stansmall.png',
+    amazonLogo: 'res/amazon_logo.png',
+    netflixLogo: 'res/netflix_logo.png',
+    dollarBill: 'res/dollar.png',
+    dude: 'res/dude.png',
+    fire: 'res/fire.png'
+};
 
-var netflixLogo = new Image();
-netflixLogo.src = 'res/netflix_logo.png';
+loadImages(sources, function(images) {
 
-var dollarBill = new Image();
-dollarBill.src = 'res/dollar.png';
+    stanLogo = images.stanLogo;
+    amazonLogo = images.amazonLogo;
+    netflixLogo = images.netflixLogo;
+    dollarBill = images.dollarBill;
+    dude = images.dollarBill;
+    fire = images.fire;
+});
 
-var dude = new Image();
-dude.src = 'res/dude.png';
-
-var fire = new Image();
-fire.src = 'res/fire.png';
+//
+// var stanLogo = new Image();
+// stanLogo.src = 'res/stansmall.png';
+//
+// var amazonLogo = new Image();
+// amazonLogo.src = 'res/amazon_logo.png';
+//
+// var netflixLogo = new Image();
+// netflixLogo.src = 'res/netflix_logo.png';
+//
+// var dollarBill = new Image();
+// dollarBill.src = 'res/dollar.png';
+//
+// var dude = new Image();
+// dude.src = 'res/dude.png';
+//
+// var fire = new Image();
+// fire.src = 'res/fire.png';

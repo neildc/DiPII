71465e83fe5c0aedfc9cc61cbd47309a519f712d
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2016-12-02 00:26:13 +1100

    forgot textures file

---
+++ scripts/textures.js
@@ -0,0 +1,18 @@
+
+var stanLogo = new Image();
+stanLogo.src = 'res/stansmall.png';
+
+var amazonLogo = new Image();
+amazonLogo.src = 'res/amazon_logo.png';
+
+var netflixLogo = new Image();
+netflixLogo.src = 'res/netflix_logo.png';
+
+var dollarBill = new Image();
+dollarBill.src = 'res/dollar.png';
+
+var dude = new Image();
+dude.src = 'res/dude.png';
+
+var fire = new Image();
+fire.src = 'res/fire.png';

8d8d0676c755fb778b3f474c2b490d26cc840965
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2017-09-18 23:26:51 +1000

    Setup webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './src',
+        publicPath: '/output'
+    }
+};
